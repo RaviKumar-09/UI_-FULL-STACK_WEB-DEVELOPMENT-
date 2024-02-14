d2c317ba0b59f4d5dc94f0dba4842522c1824b9c
RaviKumar <138367603+RAVIKUMAR-09@users.noreply.github.com>
2024-02-14 20:32:48 +0530

    Add files via upload

---
+++ JavaScript/space.js
@@ -0,0 +1,2 @@
+let person = 'Hege';
+console.log(person)
---
+++ JavaScript/syntax.js
@@ -0,0 +1,35 @@
+var x = 5;
+let y = 6;
+
+let z = x + y ;
+
+console.log(z)
+
+
+let v;
+v = 6;
+console.log(v);
+
+var k = (5+6) * 10;
+
+console.log(k);
+
+
+var r = "ravi" + " " + "kumar";
+console.log(r);
+
+
+let p,s;
+p = 10 + 1001011001;
+s = p * 88888888;
+
+console.log(p);
+console.log(s);
+
+
+const b = 10;
+const m = 100;
+
+const h = b + m 
+
+console.log(h)
